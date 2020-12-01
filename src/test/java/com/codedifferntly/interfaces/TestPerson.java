938d1ac0b6b78b3f08ddba81c66e997057f3786d
Matt Werth <matthewwerth@gmail.com>
2020-12-01 18:50:00 -0500

    Created New Test Class

--- src/test/java/com/codedifferntly/interfaces/TestPerson.java
+++ src/test/java/com/codedifferntly/interfaces/TestPerson.java
@@ -1,5 +1,33 @@
 package com.codedifferntly.interfaces;
 
-public class TestPerson {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestPerson  {
+
+    @Test
+    public void testConstructor() {
+        //GIVEN
+        Person person = new Person(123456789);
+        //WHEN
+        long expected = 123456789;
+        long actual = person.getId();
+        System.out.println(actual);
+        //THEN
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testTestSetName() {
+        //GIVEN
+        Person person = new Person(123456789);
+        person.setName("Matt");
+        //WHEN
+        String expected = "Matt";
+        String actual = person.getName();
+        System.out.println(actual);
+        //THEN
+        Assert.assertEquals(expected, actual);
+    }
 }
