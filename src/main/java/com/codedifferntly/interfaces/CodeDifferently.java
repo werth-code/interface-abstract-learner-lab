bfc7f777dd852071e334eb017ab3118f828603e2
Matt Werth <matthewwerth@gmail.com>
2020-12-04 20:05:29 -0500

    Finished Code Differently Class/Tests

--- src/main/java/com/codedifferntly/interfaces/CodeDifferently.java
+++ src/main/java/com/codedifferntly/interfaces/CodeDifferently.java
@@ -9,32 +9,35 @@ public class CodeDifferently {
     private Students students;
     private Instructors instructors;
 
+    // TODO: 12/4/20 Should we get our instances first and set them to these variables?
+
     private CodeDifferently() {
         students = Students.getInstance();
         instructors = Instructors.getInstance();
     }
 
-    // TODO: 12/4/20 Can't figure out how to iterate through these...?? Single Instance Works.
-    // casting issues..
-
     public static void hostLecture(Teacher teacher, Double numberOfHours) {
         //Take the INSTANCE and turn it into an array and then a list.
         List<Object> objList = Arrays.asList(Students.getInstance().getArray());
-
         //Create a new Array of Students that is the same size as our LIST.
         Student[] stuArray = new Student[objList.size()];
         //Use objList.toArray() to pass in our stuArray.
         objList.toArray(stuArray);
 
-        //Loop through our new Array
-        for(Student student : stuArray) {
-            System.out.println(student);
-        }
-
+        teacher.lecture(stuArray, numberOfHours);
     }
 
     public static void hostLecture(long id, Double numberOfHours) {
-////  identify a respective Instructor to host a lecture to the composite people field in the cohort reference.
+        //Take the INSTANCE and turn it into an array and then a list.
+        List<Object> objList = Arrays.asList(Students.getInstance().getArray());
+        //Create a new Array of Students that is the same size as our LIST.
+        Student[] stuArray = new Student[objList.size()];
+        //Use objList.toArray() to pass in our stuArray.
+        objList.toArray(stuArray);
+
+        Teacher teacher = (Teacher) Instructors.getInstance().findByID(id);
+
+        teacher.lecture(stuArray, numberOfHours);
     }
 
 }
--- src/test/java/com/codedifferntly/interfaces/CodeDifferentlyTest.java
+++ src/test/java/com/codedifferntly/interfaces/CodeDifferentlyTest.java
@@ -1,19 +1,44 @@
 package com.codedifferntly.interfaces;
 
-import junit.framework.TestCase;
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
 
-public class CodeDifferentlyTest extends TestCase {
+public class CodeDifferentlyTest {
 
-    @Before
-    public void setUp() {
 
-    }
-
+    @Test
     public void testHostLecture() {
         CodeDifferently.hostLecture((Teacher) Instructors.getInstance().findByID(001L), 2.0);
+
+        List<Object> objList = Arrays.asList(Students.getInstance().getArray());
+        Student[] stuArray = new Student[objList.size()];
+        objList.toArray(stuArray);
+
+        double actual = stuArray[2].getTotalStudyTime();
+        double expected = 2.0;
+        System.out.println("We are calling the hostLecture method on our Student group and all Students should now have 2.0 totalStudyTime.");
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual, 0.0);
     }
 
-    public void testTestHostLecture() {
+    @Test
+    public void testHostLectureID() {
+
+        CodeDifferently.hostLecture(001L, 2.0);
+
+        List<Object> objList = Arrays.asList(Students.getInstance().getArray());
+        Student[] stuArray = new Student[objList.size()];
+        objList.toArray(stuArray);
+
+        double actual = stuArray[2].getTotalStudyTime();
+        double expected = 2.0;
+        System.out.println("We are calling the hostLecture method from a Teacher ID on our Student group and all Students should now have 2.0 totalStudyTime.");
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual, 0.0);
+
     }
 }
